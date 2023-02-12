0cac937cec615aca3a2e2483949dfc30ba54a479
lucasclivati <lucascli@hotmail.com>
2023-02-12 16:32:48 -0300

    added exercicies arrays

---
+++ src/exercicios.js
@@ -0,0 +1,8 @@
+const catExercicios = ['Abdominal', 'Biceps', 'Costas','Ombro', 'Peito', 'Perna', 'Triceps'];
+const exercAbdominal = ['Abdominal remador', 'Abdominal no Aparelho Prancha', 'Abdominal no Smith'];
+const exercBiceps = ['Biceps polia barra reta', 'Biceps aparelho scott'];
+const exercCostas = ['Barra livre', 'Puxada alta pegada neutra', 'Puxada alta aparelho articulado', 'Remada cavalinho', 'Remaxa baixa triangulo', 'Remada no aparelho pegada fechada'];
+const exercOmbro = ['Elevação lateral', 'Elevação frontal na polia', 'Remada alta com halteres', 'Desevolvimento', 'Crucifixo invertido', 'Encolhimento'];
+const exercPeito = ['Supino inclinado c/ halteres','Supino inclinado aparelho', 'Supino reto aparelho', 'Voador', 'Crucifixo na polia banco inclinado'];
+const exercPerna = ['Agachamento livre', 'Extensora', 'Leg press', 'Agachamento pêndulo', 'Flexora sentado', 'Flexora deitado', 'Stiff', 'Panturrilha'];
+const exercTriceps = ['Triceps polia corda', 'Triceps polia barra reta'];
