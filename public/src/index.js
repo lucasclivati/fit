ee6db7207b0b28c0d25e3461f547e41acc62efd2
lucasclivati <lucascli@hotmail.com>
2023-02-18 00:07:12 -0300

    starting to convert home into javascript with functions (model to main app)

---
+++ public/src/index.js
@@ -0,0 +1,34 @@
+let exerciciosOmbro = [
+    {
+        "nome": "Elevação Lateral",
+        "ultimoPeso": 7,
+        "dataUltimoExec": "01/02/2023",
+        "unidadePeso": "kg",
+        "idTrigger": "ambos",
+    },
+    {
+        "nome": "Elevação Frontal",
+        "ultimoPeso": 8,
+        "dataUltimoExec": "01/02/2023",
+        "unidadePeso": "kg",
+        "idTrigger": "elevacaoFrontal",
+    },
+    {
+        "nome": "Elevação Unilateral",
+        "ultimoPeso": 6,
+        "dataUltimoExec": "01/02/2023",
+        "unidadePeso": "kg",
+        "idTrigger": "unilateral",
+    }
+];
+
+console.log(exerciciosOmbro);
+
+
+// function exerciciosExample(){
+//     for (let i = 0; i < exerciciosOmbro.length; i++){
+//         console.log(i);
+//     }
+// };
+
+// console.log(exerciciosExample());
