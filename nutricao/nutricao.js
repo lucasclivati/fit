8cb8fef444d8599a39c5c8dd6d1d50212c616c3e
VMarega <vinimarega@gmail.com>
2023-02-17 22:08:31 -0300

    nutrição + coisas

--- nutricao/nutricao.js
+++ nutricao/nutricao.js
@@ -5,21 +5,49 @@ const quantidade = document.querySelector("#quantidade");
 const inserir = document.querySelector("#inserirRefeicao");
 const itemDieta = document.querySelector("#itemDieta");
 const novoCard = document.createElement("div");
+const divDoNovoCard = document.createElement("div");
 let tituloRefeicao = refeicao.value;
-const novaRefeicao = document.createElement("h4");
+const novaRefeicao = document.createElement("h5");
 const novoAlimento = document.createElement("p");
 
 
 inserir.addEventListener("click", escreveHtml);
 
 function escreveHtml (){
-    itemDieta.appendChild(novoCard);
-    novoCard.appendChild(novaRefeicao);
-    novaRefeicao.textContent = refeicaoSelecionada.textContent;
+    criaDivCard();
+    criaDivCardBody();
     adicionaAlilmento();
+    insereTituloCard();
+    adicionaAlilmento();
+}
+function criaDivCard (){
+    itemDieta.appendChild(novoCard);
+    novoCard.className +="card border-secondary mb-3";
+    novoCard.style += "max-width: 18rem;";
+}
+
+function criaDivCardBody (){
+    novoCard.appendChild(divDoNovoCard);
+    divDoNovoCard.className +="card-body text-secondary";
+}
+
+function insereTituloCard() {
+    divDoNovoCard.appendChild(novaRefeicao);
+    novaRefeicao.className +="card-title";
+    novaRefeicao.textContent = refeicaoSelecionada.textContent;
 }
 
 function adicionaAlilmento(){
-    novoCard.appendChild(novoAlimento);
-    novoAlimento.textContent = alimento.value +" " + quantidade.value + "g";
+    divDoNovoCard.appendChild(novoAlimento);
+    novoAlimento.className += "card-text"
+    novoAlimento.textContent = alimento.value +" " + parseInt(quantidade.value) + "g";
 }
+
+
+/*<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>*/
