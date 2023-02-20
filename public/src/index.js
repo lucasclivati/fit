8b9dfeb33b2e3147cdbb70d02dd7b82f0a50d79f
Lucas Antonio Clivati <67926182+lucasclivati@users.noreply.github.com>
2023-02-20 12:46:39 -0300

    fixed last push

--- public/src/index.js
+++ public/src/index.js
@@ -25,27 +25,7 @@ let exerciciosOmbro = [
     }
 ];
 
-function clickarNovoPeso1 (){
-    document.getElementById("novoPeso1");
-    }
-
-salvarCarga1.addEventListener("click", () => {
-    exerciciosOmbro[0].ultimoPeso = novoPeso1.value;
-    elevacaoFrontal.click();
-    });
-
-salvarCarga2.addEventListener("click", () => {
-    exerciciosOmbro[1].ultimoPeso = novoPeso2.value;
-    unilateral.click();
-    });
-salvarCarga3.addEventListener("click", () => {
-    exerciciosOmbro[2].ultimoPeso = novoPeso3.value;
-    unilateral.click();
-    });
-
-
-console.log(exerciciosOmbro);
-
+// console.log(exerciciosOmbro);
 
 // function exerciciosExample(){
 //     for (let i = 0; i < exerciciosOmbro.length; i++){
@@ -53,4 +33,29 @@ console.log(exerciciosOmbro);
 //     }
 // };
 
-// console.log(exerciciosExample());
+
+//ao clickar no botão salvar ele: muda o peso no array, abre o proximo exercicio, clicka no input do proximo exercicio
+
+salvarCarga1.addEventListener("click", () => {
+    exerciciosOmbro[0].ultimoPeso = novoPeso1.value;
+    elevacaoFrontal.click();
+    //FAZER: adicionar automacao pra clickar automaticamente dentro do input novoPeso2
+    });
+
+salvarCarga2.addEventListener("click", () => {
+    exerciciosOmbro[1].ultimoPeso = novoPeso2.value;
+    unilateral.click();
+    //FAZER: adicionar automacao pra clickar automaticamente dentro do input novoPeso3
+    });
+salvarCarga3.addEventListener("click", () => {
+    exerciciosOmbro[2].ultimoPeso = novoPeso3.value;
+    unilateral.click();
+    });
+
+
+//ao salvar o treino, aparece uma notificação que o treino foi salvo
+salvarTreino.addEventListener("click", () => {
+    document.getElementById('notificacaoTreinoSalvo').classList.add('show');
+})
+
+
