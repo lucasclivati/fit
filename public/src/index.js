7dde97dbfeb938d1474f3792e4952f6cc85d07bc
Lucas Antonio Clivati <67926182+lucasclivati@users.noreply.github.com>
2023-02-20 15:01:39 -0300

    red matar exercicios

--- public/src/index.js
+++ public/src/index.js
@@ -35,37 +35,67 @@ let exerciciosOmbro = [
 
 listaExercicios = document.querySelectorAll('.accordion-item');
 
+botoesSalvarCarga = document.querySelectorAll('.salvarCarga');
+
 //ao clickar no botão salvar ele: muda o peso no array, abre o proximo exercicio, clicka no input do proximo exercicio
 
-salvarCarga1.addEventListener("click", () => {
-    exerciciosOmbro[0].ultimoPeso = novoPeso1.value;
+salvarCarga0.addEventListener("click", () => {
+    exerciciosOmbro[0].ultimoPeso = novoPeso0.value;
+    exerciciosOmbro[0].executado = true;
     elevacaoFrontal.click();
-    novoPeso2.focus();
+    novoPeso1.focus();
     listaExercicios[0].classList.add('border');
     listaExercicios[0].classList.add('border-success');
     });
 
-salvarCarga2.addEventListener("click", () => {
-    exerciciosOmbro[1].ultimoPeso = novoPeso2.value;
+salvarCarga1.addEventListener("click", () => {
+    exerciciosOmbro[1].ultimoPeso = novoPeso1.value;
+    exerciciosOmbro[0].executado = true;
     unilateral.click();
-    novoPeso3.focus();
+    novoPeso2.focus();
     listaExercicios[1].classList.add('border');
     listaExercicios[1].classList.add('border-success');
     });
-salvarCarga3.addEventListener("click", () => {
-    exerciciosOmbro[2].ultimoPeso = novoPeso3.value;
+
+salvarCarga2.addEventListener("click", () => {
+    exerciciosOmbro[2].ultimoPeso = novoPeso2.value;
+    exerciciosOmbro[0].executado = true;
     unilateral.click();
     listaExercicios[2].classList.add('border');
     listaExercicios[2].classList.add('border-success');
     });
 
 
+matar0.addEventListener("click", () => {
+    exerciciosOmbro[0].ultimoPeso = 0;
+    exerciciosOmbro[0].executado = false;
+    listaExercicios[0].classList.add('border', 'border-danger');
+    elevacaoFrontal.click();
+    })
+
+matar1.addEventListener("click", () => {
+    exerciciosOmbro[1].ultimoPeso = 0;
+    exerciciosOmbro[0].executado = false;
+    listaExercicios[1].classList.add('border', 'border-danger');
+    unilateral.click();
+    })
+
+matar2.addEventListener("click", () => {
+    exerciciosOmbro[2].ultimoPeso = 0;
+    exerciciosOmbro[0].executado = false;
+    listaExercicios[2].classList.add('border', 'border-danger');
+    unilateral.click();
+    })
+
 //ao salvar o treino, aparece uma notificação que o treino foi salvo
 salvarTreino.addEventListener("click", () => {
     document.getElementById('notificacaoTreinoSalvo').classList.add('show');
 })
 
-//criar uma função pra matar treino e ficar vermelho
-//criar uma função pra deixar pro final do treino
+
+//tirar os nomes fixos de id e usar posição dos botões (salvar carga, peso e matar)
 //converter tudo pra foreach
+//ao invés de apresentar através de html, fazer o html ser exibido através de array
+//criar uma função pra deixar pro final do treino
+
 
